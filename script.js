0a2d2308322c77d4cf8c31dadb2734e018a75f1d
GOKUL R <gokulrathinavel@outlook.com>
2022-04-22 17:36:08 +0530

    cm-2

--- script.js
+++ script.js
@@ -1,94 +1,73 @@
-document.getElementById("ps1").onmouseover = btn1;
-document.getElementById("ps1").onmouseout = btnc1;
+document.getElementById("ps").onmouseover = btn1;
+document.getElementById("ps").onmouseout = btnc1;
 
-document.getElementById("abt2").onmouseover = btn2;
-document.getElementById("abt2").onmouseout = btnc2;
+document.getElementById("abt").onmouseover = btn2;
+document.getElementById("abt").onmouseout = btnc2;
 
-document.getElementById("abt3").onmouseover = btn2;
-document.getElementById("abt3").onmouseout = btnc2;
 
-document.getElementById("edu1").onmouseover = btn3;
-document.getElementById("edu1").onmouseout = btnc3;
-document.getElementById("edu2").onmouseover = btn3;
-document.getElementById("edu2").onmouseout = btnc3;
-document.getElementById("edu3").onmouseover = btn3;
-document.getElementById("edu3").onmouseout = btnc3;
-document.getElementById("aoi1").onmouseover = btn3;
-document.getElementById("aoi1").onmouseout = btnc3;
+document.getElementById("edumain").onmouseover = btn3;
+document.getElementById("edumain").onmouseout = btnc3;
 
-document.getElementById("pro1").onmouseover = btn4;
-document.getElementById("pro1").onmouseout = btnc4;
-document.getElementById("pro2").onmouseover = btn4;
-document.getElementById("pro2").onmouseout = btnc4;
-document.getElementById("pro3").onmouseover = btn4;
-document.getElementById("pro3").onmouseout = btnc4;
-document.getElementById("pro4").onmouseover = btn4;
-document.getElementById("pro4").onmouseout = btnc4;
+document.getElementById("pro").onmouseover = btn4;
+document.getElementById("pro").onmouseout = btnc4;
 
-document.getElementById("tecq1").onmouseover = btn5;
-document.getElementById("tecq1").onmouseout = btnc5;
+document.getElementById("tecq").onmouseover = btn5;
+document.getElementById("tecq").onmouseout = btnc5;
 
-document.getElementById("cer1").onmouseover = btn6;
-document.getElementById("cer1").onmouseout = btnc6;
-document.getElementById("cer2").onmouseover = btn6;
-document.getElementById("cer2").onmouseout = btnc6;
-document.getElementById("cer3").onmouseover = btn6;
-document.getElementById("cer3").onmouseout = btnc6;
-document.getElementById("cer4").onmouseover = btn6;
-document.getElementById("cer4").onmouseout = btnc6;
+document.getElementById("cermain").onmouseover = btn6;
+document.getElementById("cermain").onmouseout = btnc6;
 
 function btn1(){
     let a = document.querySelector("#bt1");
-    a.setAttribute("class", "col-xxl-1 col-xl-2 col-md-2 my-md-2 col-sm-12 btn btn-dark hb active");
+    a.setAttribute("class", "col-xxl-1 col-xl-2 col-md-2 my-md-2 col-sm-12 btn btn-dark hb active btn-sm");
     }
 function btnc1(){
     let a = document.querySelector("#bt1");
-    a.setAttribute("class", "col-xxl-1 col-xl-2 col-md-2 my-md-2 col-sm-12 btn btn-dark hb");
+    a.setAttribute("class", "col-xxl-1 col-xl-2 col-md-2 my-md-2 col-sm-12 btn btn-dark hb btn-sm");
     }
 
 function btn2(){
         let a = document.querySelector("#bt2");
-        a.setAttribute("class", "col-xxl-1 col-xl-2 col-md-2 my-md-2 col-sm-12 btn btn-dark hb active");
+        a.setAttribute("class", "col-xxl-1 col-xl-2 col-md-2 my-md-2 col-sm-12 btn btn-dark hb active btn-sm");
         }
 function btnc2(){
         let a = document.querySelector("#bt2");
-        a.setAttribute("class", "col-xxl-1 col-xl-2 col-md-2 my-md-2 col-sm-12 btn btn-dark hb");
+        a.setAttribute("class", "col-xxl-1 col-xl-2 col-md-2 my-md-2 col-sm-12 btn btn-dark hb btn-sm");
         }
     
 function btn3(){
             let a = document.querySelector("#bt3");
-            a.setAttribute("class", "col-xxl-1 col-xl-2 col-md-2 my-md-2 col-sm-12 btn btn-dark hb active");
+            a.setAttribute("class", "col-xxl-1 col-xl-2 col-md-2 my-md-2 col-sm-12 btn btn-dark hb active btn-sm");
             }
 function btnc3(){
             let a = document.querySelector("#bt3");
-            a.setAttribute("class", "col-xxl-1 col-xl-2 col-md-2 my-md-2 col-sm-12 btn btn-dark hb");
+            a.setAttribute("class", "col-xxl-1 col-xl-2 col-md-2 my-md-2 col-sm-12 btn btn-dark hb btn-sm");
             }
             
             
 function btn4(){
                 let a = document.querySelector("#bt4");
-                a.setAttribute("class", "col-xxl-1 col-xl-2 col-md-2 my-md-2 col-sm-12 btn btn-dark hb active");
+                a.setAttribute("class", "col-xxl-1 col-xl-2 col-md-2 my-md-2 col-sm-12 btn btn-dark hb active btn-sm");
                 }
 function btnc4(){
                 let a = document.querySelector("#bt4");
-                a.setAttribute("class", "col-xxl-1 col-xl-2 col-md-2 my-md-2 col-sm-12 btn btn-dark hb");
+                a.setAttribute("class", "col-xxl-1 col-xl-2 col-md-2 my-md-2 col-sm-12 btn btn-dark hb btn-sm");
                 }
 
 function btn5(){
                 let a = document.querySelector("#bt5");
-                a.setAttribute("class", "col-xxl-1 col-xl-2 col-md-2 my-md-2 col-sm-12 btn btn-dark hb active");
+                a.setAttribute("class", "col-xxl-2 col-xl-4 col-md-4 my-md-2 col-sm-12 btn btn-dark hb btn-sm active");
                 }
 function btnc5(){
                 let a = document.querySelector("#bt5");
-                a.setAttribute("class", "col-xxl-1 col-xl-2 col-md-2 my-md-2 col-sm-12 btn btn-dark hb");
+                a.setAttribute("class", "col-xxl-2 col-xl-4 col-md-4 my-md-2 col-sm-12 btn btn-dark hb btn-sm");
                 }
                 
 function btn6(){
                 let a = document.querySelector("#bt6");
-                a.setAttribute("class", "col-xxl-1 col-xl-2 col-md-2 my-md-2 col-sm-12 btn btn-dark hb active");
+                a.setAttribute("class", "col-xxl-1 col-xl-2 col-md-2 my-md-2 col-sm-12 btn btn-dark hb active btn-sm");
                 }
 function btnc6(){
                 let a = document.querySelector("#bt6");
-                a.setAttribute("class", "col-xxl-1 col-xl-2 col-md-2 my-md-2 col-sm-12 btn btn-dark hb");
+                a.setAttribute("class", "col-xxl-1 col-xl-2 col-md-2 my-md-2 col-sm-12 btn btn-dark hb btn-sm");
                 }
-        
